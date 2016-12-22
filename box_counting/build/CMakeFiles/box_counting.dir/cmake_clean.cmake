4843476e2b2093a3214b968f2f65ef622276829a
Gacel <gacel.1991@gmail.com>
2016-12-22 13:37:26 +0100

    Box Counting proyecto separado v1

---
+++ box_counting/build/CMakeFiles/box_counting.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/box_counting.dir/src/main.cpp.o"
+  "box_counting.pdb"
+  "box_counting"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/box_counting.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
